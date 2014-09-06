423da2fb2f06787958ecd0b359b7daf5e3450e43
Young <littlehaker@gmail.com>
2014-09-06 16:04:13 +0800

    clear comment

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 var read = require('node-read');
 var Q = require('q');
 
-// module.exports = function(url, options) {
-//   var args = [];
-//   args.push(url);
-//   if (options) {
-//     args.push(options);
-//   }
-//   return function(fn) {
-//     args.push(fn);
-//     read.apply(this, args)
-//   }
-// }
-
 function _read(url, options) {
   var dfd = Q.defer();
 
